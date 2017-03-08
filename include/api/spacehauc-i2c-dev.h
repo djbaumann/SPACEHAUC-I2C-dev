7560a8bbc33d81b84e0b7a48d9f5a0a3c90b3c4d
David Baumann <david_baumann@student.uml.edu>
2017-03-08 18:30:25 -0500

    more updates

--- include/api/spacehauc-i2c-dev.h
+++ include/api/spacehauc-i2c-dev.h
@@ -17,14 +17,10 @@
 using std::vector;
 using std::string;
 
-namespace spacehauc-i2c {
+namespace spacehauc_i2c {
 
 /*!
- * This is a structure of integer variables x y, and z that can be easily passed
- * around by methods that measure in 3 dimensions.
- */
- /*!
- * This is a structure of floating point variables x y, and z that can be easily passed
+ * This is a structure of template variables x y, and z that can be easily passed
  * around by methods that measure in 3 dimensions.
  */
 
@@ -43,7 +39,8 @@ public:
   void setX(T X) {x = X;}
   void setY(T Y) {y = Y;}
   void setZ(T Z) {z = Z;}
-}
+};
+
 /*!
  * Enumerated data type for scaling the magnetometer. One of these is entered
  * into the magnetometer's constructor to set the scale.
@@ -53,13 +50,14 @@ enum MagScale { MAG_SCALE_2GS, MAG_SCALE_4GS, MAG_SCALE_8GS, MAG_SCALE_12GS };
 // class that contains all system I2C functions
 class I2C {
  protected:
-  virtual int openDevice(string I2C_device_name);
-  virtual int I2C_ctl(int file, i2c_rdwr_ioctl_data *packets);
+  virtual int openDevice();
+  virtual int I2C_ctl(i2c_rdwr_ioctl_data *packets);
   /*! This is an integer that represents the opened I2C file (device) */
   static int deviceFile;
+  static string filepath;
  public:
   virtual ~I2C();
-  static bool initBus(int bus);
+  static bool initBus(int busNumber);
 };
 
 /*!
@@ -69,109 +67,32 @@ class I2C {
 class I2C_Device : public I2C {
   // change inheritance for mocking
  protected:
-  /*! A vector of each address that the device may contain. */
-  vector<uint8_t> mAddress;
-  /*! A vector of each of the ID registers that the device may contain. */
-  vector<uint8_t> mID_Regsiters;
+  /*! The i2c bus address of the device*/
+  uint8_t mAddress;
   int readBytes(uint8_t reg, uint8_t *buffer, uint8_t count);
   int writeBytes(uint8_t reg, uint8_t *buffer, uint8_t count);
+  string deviceName;
+  string hexString(uint8_t decimal);
  public:
   I2C_Device();
   virtual ~I2C_Device();
-  bool initDevice();
   virtual bool init() = 0;
-  //virtual double read() = 0;
+  string getDeviceName() {return deviceName;}
 };
 
 /*!
- * This is a class for a temperature sensor, specifically the 9DoF board's
- * sensor.
- */
-class NineDoF_temp : public I2C_Device {
- private:
-  /*! A vector of each of the data registers the temperature sensor uses. */
-  vector<uint8_t> mDataRegisters;
-  /*! A vector of each of the control registers the temperature sensor uses. */
-  vector<uint8_t> mControlRegisters;
-  /*! This is a variable to hold the measured temperature value. */
-  uint8_t mTemperature;
- public:
-  explicit NineDoF_temp(int file, uint8_t address, uint8_t ID_register,
-    uint8_t controlRegister, uint8_t dataRegister);
-  ~NineDoF_temp();
-  bool init();
-  uint8_t read();
-};
-
-/*!
- * This is a class for a NineDoF_Magnetometer, specifically the 9DoF board's sensor.
- */
-class NineDoF_Magnetometer : public I2C_Device {
- private:
-  /*! An array that holds each possible scaling for the magnetometer. */
-  const float mMagScaleValue[5] = { 2 / 32768.0, 4 / 32768.0, 6 / 32768.0,
-    8 / 32768.0, 12 / 32768.0 };
-  /*! A variable of the enumerated type MagScale; is set to a scale's name.*/
-  MagScale mScale;
-  /*! A vector that holds the data register(s) for the magnetometer. */
-  vector<uint8_t> mDataRegisters;
-  /*! A vector that holds the control registers for the magnetometer. */
-  vector<uint8_t> mControlRegisters;
- public:
-  explicit NineDoF_Magnetometer(int file, uint8_t address, uint8_t ID_register,
-    uint8_t controlRegister1, uint8_t controlRegister2, uint8_t dataRegister,
-    MagScale scale);
-  ~NineDoF_Magnetometer();
-  bool init();
-  Triplet<float> read();
-};
-
-/*!
- * This is a class for a luminosity sensor.
- */
-class TSL2561 : public I2C_Device {
- private:
-  const uint8_t ID_register = 0x0A;
-  const uint8_t controlRegister1 = 0x00;
-  const uint8_t controlRegister2 = 0x01;
-  const uint8_t dataRegister = 0x0C;
-    /*! This is a variable to hold the measured luminosity value. */
-  double mLuminosity;
- public:
-  TSL2561(int file, uint8_t address);
-  ~TSL2561();
-  bool init();
-  double read();
-};
-
-class PWMcontroller : public I2C_Device {
- private:
-  /*! A vector that holds the control registers for the sensor. */
-  vector<uint8_t> mControlRegisters;
- public:
-  PWMcontroller(int file, uint8_t address, uint8_t ID_register,
-  uint8_t controlRegister1, uint8_t controlRegister2);
-  ~PWMcontroller();
-  bool initRGB_PWMcontroller();
-  bool setFreq(float freq);
-  bool channelWrite(uint8_t channel, uint16_t on, uint16_t off);
-  bool setChlPercent(uint8_t channel, uint8_t percent);
-  bool setChlDuty(uint8_t channel, float duty);
-};
-
-/*!
- * This is a class for a temperature sensor, specifically the MCP9808
- * sensor.
- */
+* This is a class for a temperature sensor, specifically the MCP9808
+* sensor.
+*/
 class MCP9808 : public I2C_Device {
- private:
+private:
   const uint8_t ID_REGISTER = 0x07;
   const uint8_t DATA_REGISTER = 0x05;
   const uint8_t CONTROL_REGISTER = 0x01;
   const uint8_t RESOLUTION_REGISTER = 0x08;
   /*! This is a variable to hold the measured temperature value. */
   double mTemperatureCelsius;
- public:
+public:
   explicit MCP9808(uint8_t address);
   ~MCP9808();
   bool init();
@@ -185,6 +106,87 @@ class MCP9808 : public I2C_Device {
     return ((mTemperatureCelsius * 1.8) + 32);
   }
 };
+
+/*!
+* This is a class for a luminosity sensor.
+*/
+class TSL2561 : public I2C_Device {
+private:
+  const uint8_t ID_register = 0x0A;
+  const uint8_t controlRegister1 = 0x00;
+  const uint8_t controlRegister2 = 0x01;
+  const uint8_t dataRegister = 0x0C;
+  /*! This is a variable to hold the measured luminosity value. */
+  double mLuminosity;
+public:
+  TSL2561(uint8_t address);
+  ~TSL2561();
+  bool init();
+  double read();
+  double getLastLuminosity() const {
+    return mLuminosity;
+  }
+};
+//
+// /*!
+//  * This is a class for a temperature sensor, specifically the 9DoF board's
+//  * sensor.
+//  */
+// class NineDoF_temp : public I2C_Device {
+//  private:
+//   /*! A vector of each of the data registers the temperature sensor uses. */
+//   vector<uint8_t> mDataRegisters;
+//   /*! A vector of each of the control registers the temperature sensor uses. */
+//   vector<uint8_t> mControlRegisters;
+//   /*! This is a variable to hold the measured temperature value. */
+//   uint8_t mTemperature;
+//  public:
+//   explicit NineDoF_temp(int file, uint8_t address, uint8_t ID_register,
+//     uint8_t controlRegister, uint8_t dataRegister);
+//   ~NineDoF_temp();
+//   bool init();
+//   uint8_t read();
+// };
+//
+// /*!
+//  * This is a class for a NineDoF_Magnetometer, specifically the 9DoF board's sensor.
+//  */
+// class NineDoF_Magnetometer : public I2C_Device {
+//  private:
+//   /*! An array that holds each possible scaling for the magnetometer. */
+//   const float mMagScaleValue[5] = { 2 / 32768.0, 4 / 32768.0, 6 / 32768.0,
+//     8 / 32768.0, 12 / 32768.0 };
+//   /*! A variable of the enumerated type MagScale; is set to a scale's name.*/
+//   MagScale mScale;
+//   /*! A vector that holds the data register(s) for the magnetometer. */
+//   vector<uint8_t> mDataRegisters;
+//   /*! A vector that holds the control registers for the magnetometer. */
+//   vector<uint8_t> mControlRegisters;
+//  public:
+//   explicit NineDoF_Magnetometer(int file, uint8_t address, uint8_t ID_register,
+//     uint8_t controlRegister1, uint8_t controlRegister2, uint8_t dataRegister,
+//     MagScale scale);
+//   ~NineDoF_Magnetometer();
+//   bool init();
+//   Triplet<float> read();
+// };
+//
+//
+// class PWMcontroller : public I2C_Device {
+//  private:
+//   /*! A vector that holds the control registers for the sensor. */
+//   vector<uint8_t> mControlRegisters;
+//  public:
+//   PWMcontroller(int file, uint8_t address, uint8_t ID_register,
+//   uint8_t controlRegister1, uint8_t controlRegister2);
+//   ~PWMcontroller();
+//   bool initRGB_PWMcontroller();
+//   bool setFreq(float freq);
+//   bool channelWrite(uint8_t channel, uint16_t on, uint16_t off);
+//   bool setChlPercent(uint8_t channel, uint8_t percent);
+//   bool setChlDuty(uint8_t channel, float duty);
+// };
+
 
 } // spacehauc-i2c
 
--- src/driver.cpp
+++ src/driver.cpp
@@ -15,20 +15,20 @@ using std::string;
 using std::cerr;
 using std::cout;
 using std::endl;
-using namespace spacehauc-i2c;
+using namespace spacehauc_i2c;
 
 
 bool testTemperatureSensor();
-bool testMagnetometer();
 bool testLuminositySensor();
-bool testRGB();
-bool setRGBColor(PWMcontroller *rgb, int red, int green, int blue);
+//bool testMagnetometer();
+//bool testRGB();
+//bool setRGBColor(PWMcontroller *rgb, int red, int green, int blue);
 
 int main(int argc, char* argv[]) {
   cout << "SPACEHAUC I2C Library Driver" << endl;
   cout << "Initializing Bus" << endl;
 
-  if (I2C::initBus(1)) == false) {
+  if (I2C::initBus(1) == false) {
     cerr << "Error: I2C bus failed to open." << endl;
   }
   cout << "Testing Temperature Sensor..." << endl;
@@ -104,7 +104,8 @@ bool testTemperatureSensor() {
   uint8_t ctlRegister = 0x24;
   uint8_t dataRegister = 0x05;
   */
-  MCP9808 tempSensor(file, 0x18);
+  MCP9808 tempSensor(0x18);
+  cout << "Initializing " << tempSensor.getDeviceName() << " Temperature Sensor..." << endl;
   if (tempSensor.init() == false) {
     cerr << "Error: Temperature Sensor failed to initalize." << endl;
     return false;
@@ -118,30 +119,8 @@ bool testTemperatureSensor() {
   return true;
 }
 
-bool testMagnetometer() {
-  uint8_t address = 0x1d;
-  uint8_t ID_register = 0x0F;
-  uint8_t ctlRegister1 = 0x25;
-  uint8_t ctlRegister2 = 0x26;
-  uint8_t dataRegister = 0x08;
-  NineDoF_Magnetometer magnetometer(file, address, ID_register, ctlRegister1,
-    ctlRegister2, dataRegister, MAG_SCALE_2GS);
-  if (magnetometer.init() == false) {
-    cerr << "Error: Magnetometer failed to initalize." << endl;
-    return false;
-  }
-  cout << "Initialized Magnetometer" << endl;
-  cout << "Reading Magnetic Field Data..." << endl;
-  for (int i = 0; i < 5; ++i) {
-    fTriplet field = magnetometer.read();
-    cout << "x: " << field.x << " y: " << field.y << " z: " << field.z << endl;
-    usleep(500000);
-  }
-  return true;
-}
-
 bool testLuminositySensor() {
-  TSL2561 light(file, 0x32);
+  TSL2561 light(0x32);
   if (light.init() == false) {
     cerr << "Error: Luminosity Sensor failed to initialize." << endl;
     return false;
@@ -154,78 +133,101 @@ bool testLuminositySensor() {
   }
   return true;
 }
-
-bool testRGB() {
-  uint8_t address = 0x40;
-  uint8_t ID_register = 1;  // We do not know what the actual register is, but
-  // it doesn't matter for now because currently id registers are not used.
-  uint8_t ctlRegister1 = 0x00;
-  uint8_t ctlRegister2 = 0x01;
-  PWMcontroller rgb(file, address, ID_register, ctlRegister1, ctlRegister2);
-  if (!rgb.initRGB_PWMcontroller()) {
-    cerr << "Error: RGB PWM controller failed to initialize" << endl;
-    return false;
-  }
-  cout << "red!" << endl;
-  if (!setRGBColor(&rgb, 100, 0, 0)) {
-    cerr << "Error: Red color failed to set" << endl;
-    return false;
-  }
-  sleep(2);
-  cout << "orange!" << endl;
-  if (!setRGBColor(&rgb, 100, 25, 0)) {
-    cerr << "Error: Orange color failed to set" << endl;
-    return false;
-  }
-  sleep(2);
-  cout << "yellow!" << endl;
-  if (!setRGBColor(&rgb, 100, 50, 0)) {
-    cerr << "Error: Yellow color failed to set" << endl;
-    return false;
-  }
-  sleep(2);
-  cout << "green!" << endl;
-  if (!setRGBColor(&rgb, 0, 100, 0)) {
-    cerr << "Error: Green color failed to set" << endl;
-    return false;
-  }
-  sleep(2);
-  cout << "cyan!" << endl;
-  if (!setRGBColor(&rgb, 0, 100, 100)) {
-    cerr << "Error: Cyan color failed to set" << endl;
-    return false;
-  }
-  sleep(2);
-  cout << "blue!" << endl;
-  if (!setRGBColor(&rgb, 0, 0, 100)) {
-    cerr << "Error: Blue color failed to set" << endl;
-    return false;
-  }
-  sleep(2);
-  cout << "purple!" << endl;
-  if (!setRGBColor(&rgb, 50, 0, 100)) {
-    cerr << "Error: Purple color failed to set" << endl;
-    return false;
-  }
-  sleep(2);
-  cout << "off!" << endl;
-  if (!setRGBColor(&rgb, 0, 0, 0)) {
-    cerr << "Error: LED failed to turn off" << endl;
-    return false;
-  }
-  return true;
-}
-
-bool setRGBColor(PWMcontroller *rgb, int red, int green, int blue) {
-  const int RED = 2;
-  const int GREEN = 3;
-  const int BLUE = 4;
-  if (rgb->setChlPercent(RED, red)) {
-    if (rgb->setChlPercent(BLUE, blue)) {
-      if (rgb->setChlPercent(GREEN, green)) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
+//
+// bool testMagnetometer() {
+//   uint8_t address = 0x1d;
+//   uint8_t ID_register = 0x0F;
+//   uint8_t ctlRegister1 = 0x25;
+//   uint8_t ctlRegister2 = 0x26;
+//   uint8_t dataRegister = 0x08;
+//   NineDoF_Magnetometer magnetometer(file, address, ID_register, ctlRegister1,
+//     ctlRegister2, dataRegister, MAG_SCALE_2GS);
+//   if (magnetometer.init() == false) {
+//     cerr << "Error: Magnetometer failed to initalize." << endl;
+//     return false;
+//   }
+//   cout << "Initialized Magnetometer" << endl;
+//   cout << "Reading Magnetic Field Data..." << endl;
+//   for (int i = 0; i < 5; ++i) {
+//     fTriplet field = magnetometer.read();
+//     cout << "x: " << field.x << " y: " << field.y << " z: " << field.z << endl;
+//     usleep(500000);
+//   }
+//   return true;
+// }
+//
+//
+// bool testRGB() {
+//   uint8_t address = 0x40;
+//   uint8_t ID_register = 1;  // We do not know what the actual register is, but
+//   // it doesn't matter for now because currently id registers are not used.
+//   uint8_t ctlRegister1 = 0x00;
+//   uint8_t ctlRegister2 = 0x01;
+//   PWMcontroller rgb(file, address, ID_register, ctlRegister1, ctlRegister2);
+//   if (!rgb.initRGB_PWMcontroller()) {
+//     cerr << "Error: RGB PWM controller failed to initialize" << endl;
+//     return false;
+//   }
+//   cout << "red!" << endl;
+//   if (!setRGBColor(&rgb, 100, 0, 0)) {
+//     cerr << "Error: Red color failed to set" << endl;
+//     return false;
+//   }
+//   sleep(2);
+//   cout << "orange!" << endl;
+//   if (!setRGBColor(&rgb, 100, 25, 0)) {
+//     cerr << "Error: Orange color failed to set" << endl;
+//     return false;
+//   }
+//   sleep(2);
+//   cout << "yellow!" << endl;
+//   if (!setRGBColor(&rgb, 100, 50, 0)) {
+//     cerr << "Error: Yellow color failed to set" << endl;
+//     return false;
+//   }
+//   sleep(2);
+//   cout << "green!" << endl;
+//   if (!setRGBColor(&rgb, 0, 100, 0)) {
+//     cerr << "Error: Green color failed to set" << endl;
+//     return false;
+//   }
+//   sleep(2);
+//   cout << "cyan!" << endl;
+//   if (!setRGBColor(&rgb, 0, 100, 100)) {
+//     cerr << "Error: Cyan color failed to set" << endl;
+//     return false;
+//   }
+//   sleep(2);
+//   cout << "blue!" << endl;
+//   if (!setRGBColor(&rgb, 0, 0, 100)) {
+//     cerr << "Error: Blue color failed to set" << endl;
+//     return false;
+//   }
+//   sleep(2);
+//   cout << "purple!" << endl;
+//   if (!setRGBColor(&rgb, 50, 0, 100)) {
+//     cerr << "Error: Purple color failed to set" << endl;
+//     return false;
+//   }
+//   sleep(2);
+//   cout << "off!" << endl;
+//   if (!setRGBColor(&rgb, 0, 0, 0)) {
+//     cerr << "Error: LED failed to turn off" << endl;
+//     return false;
+//   }
+//   return true;
+// }
+//
+// bool setRGBColor(PWMcontroller *rgb, int red, int green, int blue) {
+//   const int RED = 2;
+//   const int GREEN = 3;
+//   const int BLUE = 4;
+//   if (rgb->setChlPercent(RED, red)) {
+//     if (rgb->setChlPercent(BLUE, blue)) {
+//       if (rgb->setChlPercent(GREEN, green)) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
